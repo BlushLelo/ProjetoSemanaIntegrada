861060b2d68e19d957e7f590a6a1290693bd77e0
vntlab <vntlab@suportes-Mac-mini-23.local>
2016-07-25 17:14:56 -0300

    2-Activitys Screen

--- Semana Integrada/Semana Integrada/ActivityViewController.swift
+++ Semana Integrada/Semana Integrada/ActivityViewController.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
-class ActivyViewController: UIViewController{
+class ActivyViewController: UIViewController,UITableViewDataSource{
+    
+    let vet = ["Joao","Rodrigo","Carradas"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return vet[0]
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return vet.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath)
+        cell.textLabel?.text = vet[indexPath.row]
+        return  cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vet.count
+    }
+    
+    
+    
+    
+    
 }
--- Semana Integrada/Semana Integrada/InfoTabViewController.swift
+++ Semana Integrada/Semana Integrada/InfoTabViewController.swift
@@ -9,15 +9,13 @@
 import UIKit
 
 class InfoTabViewController : UIViewController {
-    z
-    
     
     override func viewDidLoad() {
         let storyboard = UIStoryboard(name: "Info", bundle: nil)
-        let controller = storyboard.instantiateInitialViewController();
-        addChildViewController(controller!);
-        view.addSubview(controller!.view);
-        controller!.didMoveToParentViewController(self);
+        let controller = storyboard.instantiateInitialViewController()
+        addChildViewController(controller!)
+        view.addSubview(controller!.view)
+        controller!.didMoveToParentViewController(self)
     }
     
 }
