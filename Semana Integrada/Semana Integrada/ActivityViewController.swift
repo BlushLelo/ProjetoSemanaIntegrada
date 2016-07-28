5f9c9b358c96c0661b8957bdc0756b8ab034ab58
vntlab <vntlab@suportes-Mac-mini-23.local>
2016-07-28 16:55:51 -0300

    Custom Cell With 'colors implemented'

--- Semana Integrada/Semana Integrada/ActivityViewController.swift
+++ Semana Integrada/Semana Integrada/ActivityViewController.swift
@@ -40,24 +40,31 @@ class ActivyViewController: UIViewController,UITableViewDataSource{
         let Palestra = dictionary[key]
         //cell.textLabel?.text = Palestra![indexPath.row].EventTitle
         
-        
+     
         var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as? EventTableCellView
         
         if cell == nil {
             
-            let cellNib = UINib(nibName: "EventTableCellView", bundle: nil)
+            let cellNib = UINib(nibName: "customCellInterface", bundle: nil)
             tableView.registerNib(cellNib, forCellReuseIdentifier: "myCell")
             cell = tableView.dequeueReusableCellWithIdentifier("myCell") as? EventTableCellView
             
         }
+        if ( Palestra![indexPath.row].EventType == "Palestra"){
+            cell?.eventType.textColor = UIColor(red: 0.96, green: 0.65, blue: 0.14, alpha: 1.0)
+        }
+        if ( Palestra![indexPath.row].EventType == "WorkShop"){
+            cell?.eventType.textColor = UIColor(red: 0.12, green: 0.52, blue: 0.78, alpha: 1.0)
+        }
+        cell?.eventType.text = Palestra![indexPath.row].EventType
+      
+        cell?.eventDescription.text = Palestra![indexPath.row].EventTitle
+        cell?.EventHour.text = Palestra![indexPath.row].EventLocation
         
-        cell?.myCellTitle.text = Palestra![indexPath.row].EventType
-        cell?.myCellTitleDescription.text = Palestra![indexPath.row].EventTitle
-        cell?.myCellHourDescription.text = Palestra![indexPath.row].EventLocation
         
-        return  cell!
+        return cell!
     }
-    
+
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let keys = sortedKeys
@@ -65,6 +72,8 @@ class ActivyViewController: UIViewController,UITableViewDataSource{
         let numberOfRows = dictionary[key]
         return numberOfRows!.count
     }
+    
+
     
     
     
