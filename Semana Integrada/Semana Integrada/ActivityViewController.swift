bbc9c5ff15c14df46645110909f9266e55aa4798
vntlab <vntlab@suportes-Mac-mini-23.local>
2016-07-28 08:25:28 -0300

    Schedule

--- Semana Integrada/Semana Integrada/ActivityViewController.swift
+++ Semana Integrada/Semana Integrada/ActivityViewController.swift
@@ -12,38 +12,60 @@ class ActivyViewController: UIViewController,UITableViewDataSource{
     
     //let vet = ["Joao","Rodrigo","Carradas"]
     let scheduleDAO = ScheduleDAO()
-    var dictionary = [String:Schedule]()
+    var dictionary = [String:[Schedule]]()
+    var sortedKeys = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dictionary = scheduleDAO.generatePalestras();
+        sortedKeys = Array(dictionary.keys).sort(<)
     }
-
+    
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let keys = Array(dictionary.keys)
-        return keys[section]
+        return sortedKeys[section]
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return dictionary.keys.count
+        return sortedKeys.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath)
-        let keys = Array(dictionary.keys)
+        
+        
+        //let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath)
+        let keys = sortedKeys
         let key = keys[indexPath.section]
-        let events = dictionary[key]
-        cell.textLabel?.text = events?.EventTitle
-        return  cell
+        let Palestra = dictionary[key]
+        //cell.textLabel?.text = Palestra![indexPath.row].EventTitle
+        
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as? EventTableCellView
+        
+        if cell == nil {
+            
+            let cellNib = UINib(nibName: "EventTableCellView", bundle: nil)
+            tableView.registerNib(cellNib, forCellReuseIdentifier: "myCell")
+            cell = tableView.dequeueReusableCellWithIdentifier("myCell") as? EventTableCellView
+            
+        }
+        
+        cell?.myCellTitle.text = Palestra![indexPath.row].EventType
+        cell?.myCellTitleDescription.text = Palestra![indexPath.row].EventTitle
+        cell?.myCellHourDescription.text = Palestra![indexPath.row].EventLocation
+        
+        return  cell!
     }
+    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let keys = Array(dictionary.keys)
-        let key = keys.count
-        return key
+        let keys = sortedKeys
+        let key = keys[section]
+        let numberOfRows = dictionary[key]
+        return numberOfRows!.count
     }
     
     
-
+    
 }
